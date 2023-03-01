8f5805429a96c22bfe27c98d883e84e68c6c486d
Võ Văn Đại <126147583+vovandai12@users.noreply.github.com>
2023-03-01 18:53:50 +0700

    main

---
+++ react-fe/src/components/shop/Layout.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Layout extends Component {
+    render() {
+        return (
+            <>
+                <h1> Layout Shop</h1>
+            </>
+        )
+    }
+}
+
+export default Layout;
