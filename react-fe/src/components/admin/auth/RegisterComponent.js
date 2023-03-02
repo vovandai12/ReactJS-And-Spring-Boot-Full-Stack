64548fb7f37652ac98ee7900f76e57744b09ab79
Võ Văn Đại <126147583+vovandai12@users.noreply.github.com>
2023-03-02 13:49:19 +0700

    13:49CH 02/03/2023

--- react-fe/src/components/admin/auth/RegisterComponent.js
+++ react-fe/src/components/admin/auth/RegisterComponent.js
@@ -1,29 +1,98 @@
 import React, { Component } from "react";
 import { Form, Button, Row, Col } from 'react-bootstrap';
+import { toast } from 'react-toastify';
 
 class RegisterComponent extends Component {
 
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        agree: false,
+        validated: false,
+    }
+
+    handleChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value
+        })
+    };
+
+    handleChangeEmail = (event) => {
+        this.setState({
+            email: event.target.value
+        })
+    };
+
+    handleChangePassword = (event) => {
+        this.setState({
+            password: event.target.value
+        })
+    };
+
+    handleChangeAgree = (event) => {
+        this.setState({
+            agree: event.target.checked
+        })
+    };
+
+    onSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        } else {
+            toast.success("Successfully registered. Please login to continue !")
+        }
+        this.setState({
+            validated: true
+        })
+    }
+
     render() {
+
+        const { validated } = this.state;
+
         return (
             <>
                 <Row className="mt-3">
                     <Col md={{ span: 6, offset: 3 }}>
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
+                        <Form noValidate validated={validated} onSubmit={(event) => this.onSubmit(event)}>
+                            <Form.Group controlId="username">
                                 <Form.Label>Username:</Form.Label>
-                                <Form.Control type="text" placeholder="Enter username" />
-                                <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                </Form.Text>
+                                <Form.Control type="text" placeholder="Enter username"
+                                    value={this.state.username}
+                                    onChange={(event) => this.handleChangeUsername(event)}
+                                    required />
+                                <Form.Control.Feedback type="invalid">Please enter a username.</Form.Control.Feedback>
                             </Form.Group>
 
-                            <Form.Group controlId="formBasicPassword">
+                            <Form.Group controlId="email">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email"
+                                    value={this.state.email}
+                                    onChange={(event) => this.handleChangeEmail(event)}
+                                    required />
+                                <Form.Control.Feedback type="invalid">Please enter a valid email address.</Form.Control.Feedback>
+                            </Form.Group>
+
+                            <Form.Group controlId="password">
                                 <Form.Label>Password</Form.Label>
-                                <Form.Control type="password" placeholder="Password" />
+                                <Form.Control type="password" placeholder="Enter password"
+                                    value={this.state.password}
+                                    onChange={(event) => this.handleChangePassword(event)}
+                                    required />
+                                <Form.Control.Feedback type="invalid">Please enter a password.</Form.Control.Feedback>
                             </Form.Group>
-                            <Form.Group controlId="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Check me out" />
+
+                            <Form.Group controlId="agree">
+                                <Form.Check type="checkbox" label="I agree to the terms and conditions"
+                                    checked={this.state.agree}
+                                    onChange={(event) => this.handleChangeAgree(event)}
+                                    required />
+                                <Form.Control.Feedback type="invalid">Please agree to the terms and conditions.</Form.Control.Feedback>
                             </Form.Group>
+
                             <Button variant="primary" type="submit">
                                 Submit
                             </Button>
